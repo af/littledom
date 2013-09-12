54c4bf07093dc6312d66398374000b2dc924056c
Aaron Franks <aaron.franks@gmail.com>
2013-09-12 07:31:44 -0700

    Print out failing tests in the phantomjs runner.

--- test/phantomjs_run_tests.js
+++ test/phantomjs_run_tests.js
@@ -6,15 +6,21 @@ var address = 'file://' + dir + '/test/tests.html';
 page.open(address, function () {
     // Give some time for in-page tests to run (not sure why we need so much time though):
     setTimeout(function() {
-        page.onConsoleMessage = function (msg) {
-            console.log('Browser: ' + msg);
-        };
+        page.onConsoleMessage = function (msg) { console.log(msg); };
+
         var testsPassed = page.evaluate(function() {
-              var failCount = parseInt(document.querySelector('#stats .failures em').innerText, 10);
-              var passCount = parseInt(document.querySelector('#stats .passes em').innerText, 10);
-              console.log(passCount + ' passed, ' + failCount + ' failed.');
-              return (failCount === 0) && (passCount > 0);
+            var failCount = parseInt(document.querySelector('#stats .failures em').innerText, 10);
+            var passCount = parseInt(document.querySelector('#stats .passes em').innerText, 10);
+
+            // Log all error text from mocha's html to the console:
+            var failElements = document.querySelectorAll('li.test.fail');
+            for (var i=0, l=failElements.length; i<l; i++) {
+                console.log('FAILED: ' + failElements[i].innerText + '\n');
+            }
+
+            console.log(passCount + ' passed, ' + failCount + ' failed.');
+            return (failCount === 0) && (passCount > 0);
         });
         phantom.exit(testsPassed ? 0 : 1);
-    }, 5000);
+    }, 4000);
 });
